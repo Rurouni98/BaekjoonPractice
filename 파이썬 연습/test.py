2a4fe5e6c317f1b83510e1266f38e590a6abdd7b
Rurouni98 <byungju98@gmail.com>
2025-01-26 23:37:34 +0900

    01.26 10870번 풀이 후 업로드

--- 파이썬 연습/test.py
+++ 파이썬 연습/test.py
@@ -1,4 +1,10 @@
-import math
-
-input = int(input())
-print(math.factorial(input))
+num = int(input())
+dp = [0 for i in range(num + 1)]
+dp[0] = 0
+if num >= 1:
+    dp[1] = 1
+if num >= 2:
+    dp[2] = 1
+for i in range(3, num + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+print(dp[num])
