389586753bbd16c3ece537f361ad1b5fbb217e5f
Rurouni98 <byungju98@gmail.com>
2025-02-25 23:53:47 +0900

    02.25 2440번 풀이 후 업로드

--- 파이썬 연습/test.py
+++ 파이썬 연습/test.py
@@ -1,10 +1,5 @@
 num = int(input())
-dp = [0 for i in range(num + 1)]
-dp[0] = 0
-if num >= 1:
-    dp[1] = 1
-if num >= 2:
-    dp[2] = 1
-for i in range(3, num + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
-print(dp[num])
+for i in range(0, num):
+    for j in range(i, num):
+        print("*", end = "")
+    print()
