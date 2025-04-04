8772af0154e4be6c97f8b60a935d1fb52f720e90
Rurouni98 <byungju98@gmail.com>
2025-04-04 22:15:31 +0900

    04.04 1969번 풀이 후 업로드

--- 파이썬 연습/test.py
+++ 파이썬 연습/test.py
@@ -1,18 +1,23 @@
 N, M = map(int, input().split()) # N은 DNA의 수, M은 문자열의 길이
 dnas = [input() for _ in range(N)]
-min_diff= -1
-min_dna = None
-for i in range(0, N):
-    diff_chars = 0
-    for j in range(0, N):
-        if i == j:
-            continue
-        diff_chars += sum(c1 != c2 for c1, c2 in zip(dnas[i], dnas[j]))
-    print(f"diff_chars: {diff_chars}")
-    if min_diff == -1 or min_diff > diff_chars:
-        min_diff = diff_chars
-        min_dna = dnas[i]
-    if min_diff == diff_chars and dnas[i] < min_dna:
-        min_dna = dnas[i]
-print(min_dna)
-print(min_diff)
+max_dna = []
+sum = 0
+for chars in zip(*dnas):
+    char_cnt = {}
+    for char in chars:
+        if char in char_cnt:
+            char_cnt[char] += 1
+        else:
+            char_cnt[char] = 1
+
+    max_count = 0
+    max_char = ''
+
+    for char in sorted(char_cnt.keys()):
+        if char_cnt[char] > max_count:
+            max_count = char_cnt[char]
+            max_char = char
+    max_dna.append(max_char)
+    sum += N - max_count
+print(''.join(max_dna))
+print(sum)
