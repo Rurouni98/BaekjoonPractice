9bcb60b565e11f62205c1b251078d186ac668b5c
Rurouni98 <byungju98@gmail.com>
2025-04-06 23:46:51 +0900

    2217번 풀이중

--- 파이썬 연습/test.py
+++ 파이썬 연습/test.py
@@ -1,23 +1,3 @@
-N, M = map(int, input().split()) # N은 DNA의 수, M은 문자열의 길이
-dnas = [input() for _ in range(N)]
-max_dna = []
-sum = 0
-for chars in zip(*dnas):
-    char_cnt = {}
-    for char in chars:
-        if char in char_cnt:
-            char_cnt[char] += 1
-        else:
-            char_cnt[char] = 1
+N = int(input()) # N은 로프의 갯수
+ropes = [input() for _ in range(N)]
 
-    max_count = 0
-    max_char = ''
-
-    for char in sorted(char_cnt.keys()):
-        if char_cnt[char] > max_count:
-            max_count = char_cnt[char]
-            max_char = char
-    max_dna.append(max_char)
-    sum += N - max_count
-print(''.join(max_dna))
-print(sum)
