592a1382043b5b15611d5c8528194801dfb07cbe
Rurouni98 <byungju98@gmail.com>
2025-05-03 02:00:36 +0900

    05.03 13172번 풀이 후 업로드

--- 파이썬 연습/test.py
+++ 파이썬 연습/test.py
@@ -1,9 +1,13 @@
-people = int(input())
-tips = [int(input()) for _ in range(people)]
-tips.sort(reverse=True)
-maxSum = 0
-diff = 0
-for tip in tips:
-    maxSum += (tip - diff) if tip - diff >= 0 else 0
-    diff += 1
-print(maxSum)
+def modinv(b, mod):
+    return pow(b, mod - 2, mod)
+
+def modfrac(numer, denom, mod):
+    return (numer * modinv(denom, mod)) % mod
+
+M = int(input()) # M은 주사위의 갯수
+total = 0
+for i in range(M):
+    N, S = map(int, input().split()) # N은 몇 면체인지, S는 모든 면에 적힌 수를 더한 값
+    total += modfrac(S, N, 1000000007)
+
+print(total % 1000000007)
